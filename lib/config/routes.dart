6dcbf1fb7b767d3f14c660a2911115bd18dbf3f8
AtharvMixraw <antilogatharv@gmail.com>
2025-05-25 13:50:16 +0530

    Remove unwanted files

--- lib/config/routes.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:flutter/material.dart';
-// import '../presentation/screens/home_screen.dart';
-// import '../presentation/screens/splash_screen.dart';
-
-// final Map<String, WidgetBuilder> appRoutes = {
-//   '/': (context) => const SplashScreen(),
-//   '/home': (context) => const HomeScreen(),
-// };
